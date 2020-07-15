c63c5fe07222c1930b70fc75cab4b18f96e8d8f3
TuralHasanov <hasanovtural11@gmail.com>
2020-07-15 15:13:20 +0400

    images changes for views 2

--- resources/js/components/chat/ContactList.vue
+++ resources/js/components/chat/ContactList.vue
@@ -9,8 +9,14 @@
             >
 
                 <div class="media border bg-white p-2 text-muted .contact">
-                    <img v-if="contact.role=='d'" :src="`/storage/images/doctors/${contact.profile_image?contact.profile_image:'default_user.jpg'}`" width="56" height="56" class="mr-3 rounded-circle" alt="...">
-                    <img v-else :src="`/storage/images/users/${contact.profile_image?contact.profile_image:'user_default.png'}`" width="56" height="56" class="mr-3 rounded-circle" alt="...">
+                    <img v-if="contact.role=='d'" src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bluecrossnc.com%2Fmanage-your-plan&psig=AOvVaw2iYe9Il0BLFCmDG7AwJjCZ&ust=1594889453853000&source=images&cd=vfe&ved=0CAIQjRxqGAoTCIDc-cTwzuoCFQAAAAAdAAAAABCIAQ' width="3em" height="3em" class="mr-3 rounded-circle" alt="...">
+                     <template v-else class="text-primary">
+                        <svg width="3em" height="3em" style="color:#3490dc;" viewBox="0 0 16 16" class="bi bi-person-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
+                            <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                            <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
+                        </svg>
+                    </template>
                     <div class="media-body">
                       <h6 class="mt-0">
                             <strong style="word-wrap:break-word" class="text-gray-dark">{{contact.name}}</strong>
