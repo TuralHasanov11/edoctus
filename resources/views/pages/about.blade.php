2f4d4c07cd338746658344d1222c74970ccd7cb4
TuralHasanov <hasanovtural11@gmail.com>
2020-07-29 16:21:13 +0400

    change about page 2

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -10,24 +10,25 @@
           <h2 class="contact-title text-center">Haqqımızda</h2>
         </div>
         <div class="col">
-            <div class="row justify-content-between">
-              <div class="col-12 col-md-5">
-                <b>Medicus</b> - beynəlxalq <a target="_blank" href="https://enactus.org/">Enactus</a> yarışı çərçivəsində irəli sürülmüş 
-                və <a target="_blank" href="http://www.bhos.edu.az/az/page/722-innovasiya-va-tadqiqatlar-markazi/staff">BANM İnnovasiya və Tədqiqatlar Mərkəzinin</a> dəstəyi ilə <a target="_blank" href="http://www.bhos.edu.az/">Baki Ali Neft Məktəbinin</a> tələbələri tərəfindən veb-səhifə olaraq hazırlanmışdır. 
-                Bir komanda olaraq, bizim məqsədimiz pandemiya dövründə insanları 
-                Covid-19 barədə düzgün məlumatlandırmaq və xəstəliyin ilkin simptomları aşkarlandığı halda həkimlərlə 
-                onlayn şəkildə əlaqə də ola biləcəkləri platforma yaratmaqdır. 
-                İnanırıq ki, təqdim etdiyimiz veb-səhifə insanları virusdan necə qorunmaq və virusa yoluxduqları 
-                halda hansı tədbirləri həyata keçirməklə bağlı maarifləndirəcək və cəmiyyətin virusla mübarizəsində 
-                güclü müdafiə rolu oynayacaqdır. Həmçinin istifadəçilərin müəyyən məsələlərlə bağlı sualları yarandıqda,
-                bu suallar platformamızda paylaşmaqla həkimlər və ya digər istifadəçilər tərəfindən cavablandırıla bilər 
-              </div>
-              <div class="col-12 col-md-6">
-                  <img class="d-block my-3" src="https://firebasestorage.googleapis.com/v0/b/medicusv1.appspot.com/o/images%2Fabout%2Fpp.JPG.jfif?alt=media&token=f7a06ef7-a28e-4fdb-bb45-4cea4c73bf65" alt="Enactus">
-                  <hr>
-                  <img class="d-block my-3" src="https://firebasestorage.googleapis.com/v0/b/medicusv1.appspot.com/o/images%2Fabout%2Funnamed.jpg?alt=media&token=36b86f52-748c-4a49-8e3e-01c9b5a67a51" alt="">
-              </div>
+          <div class="row justify-content-between ">
+            <div class="col-12 col-md-5">
+              <b>Medicus</b> - beynəlxalq <a target="_blank" href="https://enactus.org/">Enactus</a> yarışı çərçivəsində irəli sürülmüş 
+              və <a target="_blank" href="http://www.bhos.edu.az/az/page/722-innovasiya-va-tadqiqatlar-markazi/staff">BANM İnnovasiya və Tədqiqatlar Mərkəzinin</a> dəstəyi ilə <a target="_blank" href="http://www.bhos.edu.az/">Baki Ali Neft Məktəbinin</a> tələbələri tərəfindən veb-səhifə olaraq hazırlanmışdır. 
+              Bir komanda olaraq, bizim məqsədimiz pandemiya dövründə insanları 
+              Covid-19 barədə düzgün məlumatlandırmaq və xəstəliyin ilkin simptomları aşkarlandığı halda həkimlərlə 
+              onlayn şəkildə əlaqə də ola biləcəkləri platforma yaratmaqdır. 
+              İnanırıq ki, təqdim etdiyimiz veb-səhifə insanları virusdan necə qorunmaq və virusa yoluxduqları 
+              halda hansı tədbirləri həyata keçirməklə bağlı maarifləndirəcək və cəmiyyətin virusla mübarizəsində 
+              güclü müdafiə rolu oynayacaqdır. Həmçinin istifadəçilərin müəyyən məsələlərlə bağlı sualları yarandıqda,
+              bu suallar platformamızda paylaşmaqla həkimlər və ya digər istifadəçilər tərəfindən cavablandırıla bilər 
             </div>
+            <div class="col-12 col-md-7">
+                <div class="row align-items-center">
+                  <div class="col-12 col-md-6"><img class="my-3 float-left" src="https://firebasestorage.googleapis.com/v0/b/medicusv1.appspot.com/o/images%2Fabout%2Fpp.JPG.jfif?alt=media&token=f7a06ef7-a28e-4fdb-bb45-4cea4c73bf65" alt="Enactus"></div>
+                  <div class="col-12 col-md-6"><img class="my-3 float-left" src="https://firebasestorage.googleapis.com/v0/b/medicusv1.appspot.com/o/images%2Fabout%2Funnamed.jpg?alt=media&token=36b86f52-748c-4a49-8e3e-01c9b5a67a51" alt="Bkı Ali Neft Məktəbi"></div>
+                </div>
+            </div>
+        </div>
             <p></p>
         </div>
         
