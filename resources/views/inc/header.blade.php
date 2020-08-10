b04e10b76ce979bab8d839666e82c304eb1d61de
TuralHasanov <hasanovtural11@gmail.com>
2020-08-10 18:56:53 +0400

    finishing touches 2

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -4,7 +4,7 @@
         <div class="row align-items-center">
             <div class="col-lg-12">
                 <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand" href="/"> <img src="https://firebasestorage.googleapis.com/v0/b/medicusv1.appspot.com/o/images%2Flogo_2.PNG?alt=media&token=78982f6c-1bbc-45e0-aa2b-6af385e7ef17" alt="logo"> </a>
+                    <a class="navbar-brand" href="/"> <img src="https://firebasestorage.googleapis.com/v0/b/medicusv1.appspot.com/o/images%2Flogo_2_new.png?alt=media&token=301ff3ff-11bb-4c43-b19c-fa7595c1cdcf" alt="logo"> </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
