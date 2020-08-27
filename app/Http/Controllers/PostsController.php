b1d67cc49ca4b84e2635e9c43aca3b1fa4a61d17
TuralHasanov <hasanovtural11@gmail.com>
2020-08-27 21:24:41 +0400

    posts error handling 1

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -19,6 +19,7 @@ class PostsController extends Controller
     public function index()
     {
         $posts=Post::with('user')->where('approved',1)->orderBy('created_at', 'desc')->paginate(5);
+        dd($posts);
 
         return view('posts.index')->with('posts', $posts);
     }
